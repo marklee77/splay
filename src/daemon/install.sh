a21ff51a9a2c565e0f0d54342eab373f021c8356
Lucas Charles <lucas.charles@unine.ch>
2011-11-09 14:48:30 +0000

    support for native libs

--- src/daemon/install.sh
+++ src/daemon/install.sh
@@ -55,6 +55,7 @@ cp splay_core.so $L_CPATH/
 cp luacrypto/crypto.so $L_CPATH/crypto.so
 cp misc_core.so $L_CPATH/splay/
 cp data_bits_core.so $L_CPATH/splay/
+cp lbase64/base64.so $L_CPATH/base64.so
 
 echo
 echo
